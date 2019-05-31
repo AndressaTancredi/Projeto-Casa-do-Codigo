9146b09338790182381e559eb70859e7be7dce74
Andressa Tancredi <andressa.tancredi@hotmail.com>
2019-05-31 13:26:18 -0300

    Atendendo rotas usando if e else

--- server.js
+++ server.js
@@ -1,26 +1,33 @@
-//Módulo Node é uma biblioteca, um conjunto de funcionalidades. Um arquivo .js é um módulo.
-
-// Como importar um módulo do próprio Node:
 const http = require('http');
 
-//Executar o método createServer para criar o servidor. Que vai retornar um server.
-const servidor = http.createServer(function (req, resp) { //O que ele vai fazer quando receber a requisição que no caso o conteúdo para o cliente:
-    resp.end(` 
-    <html>
-        <head>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <h1> Casa do Código </h1>
-        </body> 
-    /html>`
-    );
+//Será que esses if e else é a melhor abordagem?
+const servidor = http.createServer(function (req, resp) { 
+    let html = "";
+
+    if (req.url =='/'){ 
+        html = ` 
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1> Casa do Código </h1>
+            </body> 
+        </html>`
+        ;
+    } else if (req.url == '/livros'){
+        html = ` 
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1> Listagem de Livros </h1>
+            </body> 
+        </html>`
+        ;
+    }
+    // Outros else if...
+    resp.end(html);
 });
-
-//Qual vai ser a porta que meu servidor vai funcionar:
-servidor.listen(3000); // Servidor escute a porta 3000.
-
-
-
-
-
+servidor.listen(3000);
