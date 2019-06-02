53c763f51a8efe659e90e3d0e43eb86c812dce87
Andressa Tancredi <andressa.tancredi@hotmail.com>
2019-06-02 14:07:30 -0300

    Torna a listagem de livros dinâmica e acessa o BD

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,3 +1,5 @@
+const db = require('../../config/database');
+
 module.exports = (app) => {
     app.get('/', function(req,resp) {
         resp.send( 
@@ -14,22 +16,15 @@ module.exports = (app) => {
         );
     });
     
-    //Quero carregar a lista.marko e carregar o valor da lista de array que eu defini - depois uso a marko expression no lista.marko
     app.get('/livros', function(req, resp) {
-        resp.marko(
-            require('../views/livros/lista/lista.marko'),
-            {
-                livros: [
-                    { 
-                        id: 1,
-                        titulo: 'Fundamentos do Node'
-                    },
-                    { 
-                        id: 2,
-                        titulo: 'Node Avançado'
-                    }
-                ]
-            }
-        );
+        db.all('SELECT * FROM livros', function(erro, resultados){
+
+            resp.marko(
+                require('../views/livros/lista/lista.marko'),
+                {
+                    livros: resultados
+                }
+            );
+        });
     });
 };
